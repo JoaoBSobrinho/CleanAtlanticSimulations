0053c79b6bc269d98d1f82e6544a64ace4d4c424
Daniel Garaboa <angeldaniel.garaboa@usc.es>
2020-06-02 12:01:52 +0200

    Split OceanSources Cases

---
+++ Atlantic_OceanSources_MarineTraffic/RunCase.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+clear
+
+# Read the MOHIDLagrangianPath
+source ../MOHIDLagrangianPath.sh
+
+# "name" and "dirout" are named according to the testcase
+name=${PWD##*/}_case
+dirout=${name}_out
+
+# "executables" are renamed and called from their directory
+tools=${MOHIDLagrangianPath}/build/bin
+mohidlagrangian=${tools}/MOHIDLagrangian
+
+preprocessorDir=${MOHIDLagrangianPath}/src/MOHIDLagrangianPreProcessor
+mohidPreprocessor=${preprocessorDir}/MOHIDLagrangianPreProcessor.py
+
+postProcessorDir=${MOHIDLagrangianPath}/src/MOHIDLagrangianPostProcessor
+mohidPostprocessor=${postProcessorDir}/MOHIDLagrangianPostProcessor.py
+
+# "dirout" is created to store results or it is cleaned if it already exists
+if [ -e $dirout ]; then
+  rm -f -r $dirout
+fi
+mkdir $dirout
+
+cp ${name}.xml $dirout/
+
+# CODES are executed according the selected parameters of execution in this testcase
+
+python $mohidPreprocessor -i $dirout/${name}.xml -o $dirout
+
+errcode=0
+if [ $errcode -eq 0 ]; then
+  $mohidlagrangian -i $dirout/${name}.xml -o $dirout
+  errcode=$?
+fi
+
+python -W ignore $mohidPostprocessor -i ${name}.xml -o $dirout
+
+if [ $errcode -eq 0 ]; then
+  echo All done
+else
+  echo Execution aborted
+fi
+read -n1 -r -p "Press any key to continue..." key
+echo
