f4fbcc01146cb3570bf1a0a6b01618310b74098c
Daniel Garaboa <angeldaniel.garaboa@usc.es>
2020-06-08 18:43:22 +0200

    Update Script to create xml form csv

--- scripts/csvFilesToMOHIDsources.py
+++ scripts/csvFilesToMOHIDsources.py
@@ -11,7 +11,6 @@ import pandas as pd
 import argparse
 
 def indent(elem, level=1):
-    
     i = "\n" + level*"  "
     j = "\n" + (level-1)*"  "
     if len(elem):
@@ -26,35 +25,31 @@ def indent(elem, level=1):
     else:
         if level and (not elem.tail or not elem.tail.strip()):
             elem.tail = j
-    return elem 
+    return elem
 
 
 def csvRowToMOHIDDischargeFileSourceElemXML(root,source_id,name,x,y,z,rateFile):
-  
     doc = ET.SubElement(root, "source")
     ET.SubElement(doc, "setsource", id=str(source_id),name=str(name))
     RateFileElement=ET.SubElement(doc, "rateTimeSeries")
     ET.SubElement(RateFileElement,"file",comment="name of csv file with discharge information (time and rate columns)",name=str(rateFile))
     ET.SubElement(RateFileElement,"scale",comment="scales the data on the file by this factor (not time)",value="1",)
     ET.SubElement(doc, "point",units_comment="(deg,deg,m)", x=str(x),y=str(y),z=str(z))
-
     return root
 
 
-def csvRowToMOHIDPointSourceElemXML(root,source_id,name,x,y,z,rate,start,end):
-  
+def csvRowToMOHIDPointSourceElemXML(root,source_id,name,x,y,z,rate,start=0,end='end'):
     doc = ET.SubElement(root, "source")
     ET.SubElement(doc, "setsource", id=str(source_id),name=name)
     ET.SubElement(doc, "rate", value=str(rate), comment="emission rate (Hz)")
-    ET.SubElement(doc, "active", start=start, end =end)
+    ET.SubElement(doc, "active", start=str(start), end =str(end))
     ET.SubElement(doc, "point", x=str(x),y=str(y),z=str(z),units_comment="(deg,deg,m)")
-    
     return root
 
 def csvRowToMOHIDTypeSourceElemXML(root,i,materialType,properties):
-  
+
     ET.SubElement(root, "type", source=str(i),type=materialType,property=str(properties))
-   
+
     return root
 
 
@@ -83,6 +78,7 @@ def toMOHIDLagrangianEmisorFile(df,fileName,timeUnits='seconds',scale=1):
     f.write('<EndTimeSerie>')
     f.close()
 
+
 def main():
     """
     to run the conversor is python main.py -f filename.csv
@@ -92,7 +88,7 @@ def main():
     source_id,name,x,y,z,rate,start,end
     1,'name_or_source_1',0,10,0,1,1,1000
     2,'name_or_source_2',10,10,0,1,1,1000
-    
+
     	<type source="2" type='plastic' property="bag_1" comment="" />
 		<type source="3" type='paper' property="cardboard_1" comment="" />
     ...
@@ -100,54 +96,72 @@ def main():
     parser = argparse.ArgumentParser(description='CSV point file to MOHID Sources XML block')
     parser.add_argument("-f", "--file", dest="csvFilename", help="csv file style with columns names: source_id,name,x,y,z,rate,start,end")
     args = parser.parse_args()
-    
-    
+
+
     # Start the root for the MOHIDLagrangian setup
     xmlMOHIDtree = ET.Element("root")
-    
+
     # Start the source definitions block
-    sourceDefinitions = ET.SubElement(xmlMOHIDtree,"sourceDefinitions")
-    df = pd.read_csv(args.csvFilename,delimiter=';')
+    sourceDefinitions = ET.SubElement(xmlMOHIDtree, "sourceDefinitions")
+    df = pd.read_csv(args.csvFilename, delimiter=',')
     if 'file' in df:
         sourceId = 0
-        for index,row in df.iterrows():
-            if 'type' in row:  
+        for index, row in df.iterrows():
+            if 'type' in row:
                 materialTypes = row['type'].split('-')
                 properties = row['property'].split('-')
+                k = 0
                 for materialType in materialTypes:
-                    sourceDefinitions = csvRowToMOHIDDischargeFileSourceElemXML(sourceDefinitions,sourceId,row['name']+'_'+materialType,row.x,row.y,row.z,'data/'+row['file'])
-                    print('Adding source',row.name)
+                    source_name = row['name'] + '_' + materialTypes[k] + '_' + properties[k]
+                    sourceDefinitions = csvRowToMOHIDDischargeFileSourceElemXML(sourceDefinitions,
+                                                                                sourceId, source_name,
+                                                                                row.x, row.y, row.z,
+                                                                                'data/'+row['file'])
+                    print('-> Adding file source', row.name)
                     sourceId = sourceId+1
+                    k = k+1
             else:
-                    sourceDefinitions = csvRowToMOHIDDischargeFileSourceElemXML(sourceDefinitions,sourceId,row['name'],row.x,row.y,row.z,'data/'+row['file'])
-                    print('Adding source',row.name)
-                    sourceId = sourceId+1
+                sourceDefinitions = csvRowToMOHIDDischargeFileSourceElemXML(sourceDefinitions,
+                                                                            sourceId,row['name'],
+                                                                            row.x,row.y,row.z,
+                                                                            'data/'+row['file'])
+                print('-> Adding file source', row.name)
+                sourceId = sourceId+1
 
-    else:    
+    else:
         sourceId = 0
-        for index,row in df.iterrows():
-            if 'type' in row:  
+        for index, row in df.iterrows():
+            if 'type' in row:
                 materialTypes = row['type'].split('-')
+                properties = row['property'].split('-')
             else:
                 materialTypes = []
+            k=0
             for materialType in materialTypes:
-                sourceDefinitions = csvRowToMOHIDPointSourceElemXML(sourceDefinitions,sourceId,row.name,row.x,row.y,row.z,row.start,row.end)
-                print('Adding source',row)
+                source_name = row['name'] + '_'+ materialTypes[k] + '_' + properties[k]
+                sourceDefinitions = csvRowToMOHIDPointSourceElemXML(sourceDefinitions,
+                                                                    sourceId, source_name,
+                                                                    row.x, row.y, row.z,
+                                                                    row.rate)
+                print('-> Adding point source', row)
                 sourceId = sourceId+1
-    
+                k=k+1
+
     # Start the type definitions block
     typesDefinitions = ET.SubElement(xmlMOHIDtree,"types")
     sourceId = 0
-    for index,row in df.iterrows():
-            k = 0
-            materialTypes = row['type'].split('-')
-            properties = row['property'].split('-')
-            for materialType in materialTypes:
-                typesDefinitions = csvRowToMOHIDTypeSourceElemXML(typesDefinitions,sourceId,materialTypes[k],properties[k])
-                print('Adding source',row.name)
-                k = k+1
-                sourceId = sourceId+1
-    
+    for index, row in df.iterrows():
+        k = 0
+        materialTypes = row['type'].split('-')
+        properties = row['property'].split('-')
+        for materialType in materialTypes:
+            typesDefinitions = csvRowToMOHIDTypeSourceElemXML(typesDefinitions,
+                                                              sourceId,
+                                                              materialTypes[k],
+                                                              properties[k])
+            print('Adding types', row['name'])
+            k = k+1
+            sourceId = sourceId+1
 
     indent(xmlMOHIDtree)
     tree = ET.ElementTree(xmlMOHIDtree)
