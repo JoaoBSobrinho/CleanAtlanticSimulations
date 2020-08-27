6e518a074c64cc44e506132c7c9f49520765f69e
DanielGaraboaPaz <angeldaniel.garaboa@usc.es>
2020-08-27 18:40:01 +0200

    Add RunCase with optional dirout

--- Portugal_OceanSources_MarineTraffic/RunCase.sh
+++ Portugal_OceanSources_MarineTraffic/RunCase.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 clear
 
 # increase stacksize
@@ -11,7 +11,14 @@ source ../MOHIDLagrangianPath.sh
 
 # "name" and "dirout" are named according to the testcase
 name=${PWD##*/}_case
-dirout=${name}_out
+#if no dirout is provided, create on local
+
+if [ -z ${dirout+x} ]; 
+then 
+    dirout=${name}_out
+else 
+    dirout=${dirout}${name}_out
+fi
 
 # "executables" are renamed and called from their directory
 tools=${MOHIDLagrangianPath}/build/bin
@@ -27,7 +34,7 @@ mohidPostprocessor=${postProcessorDir}/MOHIDLagrangianPostProcessor.py
 if [ -e $dirout ]; then
   rm -f -r $dirout
 fi
-mkdir $dirout
+mkdir -p $dirout
 
 cp ${name}.xml $dirout/
 
