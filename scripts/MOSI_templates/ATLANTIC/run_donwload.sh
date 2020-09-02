3599a214295a620a026d5f14e4b699357a354513
DanielGaraboaPaz <angeldaniel.garaboa@usc.es>
2020-09-02 23:37:05 +0200

    correct winds name

--- scripts/MOSI_templates/ATLANTIC/run_donwload.sh
+++ scripts/MOSI_templates/ATLANTIC/run_donwload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python -m MOSI -i CDS_winds_2D.json -s CDS -f 1d
+python -m MOSI -i CDS_winds.json -s CDS -f 1d
 python -m MOSI -i CMEMS_currents_2D.json -s CMEMS -f 1d
 python -m MOSI -i CMEMS_currents_3D.json -s CMEMS -f 1d
 python -m MOSI -i CMEMS_salt.json -s CMEMS -f 1d
