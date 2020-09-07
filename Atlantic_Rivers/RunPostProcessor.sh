4686f4b84509515c015c299f9a9860f8bbd0aa06
DanielGaraboaPaz <angeldaniel.garaboa@usc.es>
2020-09-07 17:08:44 +0200

    Update path recipes

--- Atlantic_Rivers/RunPostProcessor.sh
+++ Atlantic_Rivers/RunPostProcessor.sh
@@ -15,13 +15,6 @@ else
     dirout=${dirout}${name}_out
 fi
 
-if [ -z ${dirout+x} ]; 
-then 
-    dirout=${name}_out
-else 
-    dirout=${dirout}${name}_out
-fi
-
 
 # "executables" are renamed and called from their directory
 
